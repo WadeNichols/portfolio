1b57fcb1e34c05daf29598b08ff8f3639b36d50b
Wade Nichols <kwnichols73@gmail.com>
2018-12-06 10:25:08 -0800

    working on header

--- src/Header.js
+++ src/Header.js
@@ -6,8 +6,10 @@ function Header() {
             <h1>Wade Nichols</h1>
             <p>8147 Weeping Willow Lane</p>
             <p>Sacramento, CA. 95828</p>
-            <a type="email" href='mailto:kwnichols73@gmail.com?&subject=Resume%20Request'>kwnichols73@gmail.com</a>
+            <a type="email" href='mailto: wnichols73@gmail.com?&subject=Resume%20Request'>kwnichols73@gmail.com</a>
+            <i></i>
         </div>
+
     )
 }
 
