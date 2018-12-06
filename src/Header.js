3b34899a1a741a917b0cd77d457066a84a5a5dfe
Wade Nichols <kwnichols73@gmail.com>
2018-12-06 11:20:47 -0800

    added contact icons to header

--- src/Header.js
+++ src/Header.js
@@ -3,11 +3,12 @@ import React from 'react'
 function Header() {
     return (
         <div className='header'>
-            <h1>Wade Nichols</h1>
-            <p>8147 Weeping Willow Lane</p>
-            <p>Sacramento, CA. 95828</p>
-            <a type="email" href='mailto: wnichols73@gmail.com?&subject=Resume%20Request'>kwnichols73@gmail.com</a>
-            <i></i>
+            <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.5.0/css/all.css' integrity='sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU' crossorigin='anonymous'></link>
+            <h1 className="name">Wade Nichols</h1>
+            <p>8147 Weeping Willow Lane<br/>Sacramento, CA. 95828<br/></p>
+            <a target="_blank" href="https://github.com/WadeNichols"><i className="fab fa-github"></i></a>
+            <a target="_blank" type="email" href='mailto: wnichols73@gmail.com?&subject=Resume%20Request'><i class='fas fa-envelope'></i></a>
+            <a href="https://www.linkedin.com/in/wade-nichols-9bb33a164/"><i class='fab fa-linkedin'></i></a>
         </div>
 
     )
