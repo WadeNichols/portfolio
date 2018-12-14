100ecdc610ec0dc5f1d3f3b12deff046024cb546
Wade Nichols <kwnichols73@gmail.com>
2018-12-14 13:42:53 -0800

    about me rough draft

--- src/About.js
+++ src/About.js
@@ -4,14 +4,49 @@ export default function About() {
   return (
     <div>
       <header>
-        <h1>Have You Met Wade?</h1>
+        <h1>Haaaaaaaaave You Met Wade?</h1>
       </header>
       <body>
+        <h6>
+          Hi, I'm Wade and this is the story of how I met your mother.
+          <br />
+          Oops, thats the wrong story; this is a explanation to why you might be
+          reading this.
+        </h6>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dolor
-          deleniti est ducimus iure culpa voluptate qui odit doloremque,
-          praesentium fuga deserunt architecto sunt? Veniam illo rerum aliquam
-          impedit repudiandae.
+          A lot of my past work experience were jobs that just got me by. They
+          were jobs such as paper routes, working in restaurants, customer
+          service and, most recently, construction. None of these meant a whole
+          lot to me and . It's not that i hated any of the jobs or the work i did but
+          i didn't have any passion for them. I had them to pay the rent or, when
+          i was younger, to buy my toys. Electronics were always something i was
+          interested in; now days i can spend hours in Best Buy just looking at
+          their products, especially the computers/tablets and phones. When i
+          was 14 years old and had my own paper route, one of the things i
+          bought was my very own desktop. I had saved up for a while and when i
+          finally was able to purchase it i was like a kid in a candy shop. I
+          loved being on my computer and just messing around with it and i
+          became very familiar with it.
+        </p>
+        <p>
+          I never did anything with that interest after high school and i always
+          regretted it. As i kept taking on more and more responsibilities it
+          seemed to become more and more difficult to go back to school to
+          pursue my interest in technology, specifically computers. As time kept
+          going it kept weigh me down that i never did anymore education after
+          HS and i was starting to give up on the idea when one day i got the
+          push i needed to do something to better myself and to better my life.
+          So i did my research and once i mad the decision i quit my job the
+          next day so that i could attend a Fullstack Web Developer boot camp in
+          Salt Lake City, UT. my home town. I spent three months at this full
+          time boot camp going from knowing nothing about coding to having the
+          knowledge and tools i need to do something better with my life and
+          pursue my interests that i nearly left behind.
+        </p>
+        <p>
+        So this is where i am at now; I am enthused to begin this new journey in my life and doing something that gives me joy or drives me out of my mind but that can be fixed with a 15 minute walk.<br/>
+          Graduating V-School is a major accomplishment in my life and I'm proud of myself. I didn't think coding would be easy but i soon discovered that this was going to be a challenge, so: CHALLENGE ACCEPTED! I am excited to embrace this new chapter and see where its going to take me.
+          Feel free to poke around my portfolio to see a little of what i learned.
         </p>
       </body>
     </div>
