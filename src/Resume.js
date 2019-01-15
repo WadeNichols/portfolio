7726a88dc18f733d000e004d3b35007813a3598e
Wade Nichols <kwnichols73@gmail.com>
2019-01-15 12:52:46 -0800

    fixed resume download

--- src/Resume.js
+++ src/Resume.js
@@ -15,7 +15,7 @@ function Resume() {
 
       <h1 className="resume-title">Here's My Resume, Check It Out.</h1>
 
-      <a className="resume-download" href={resume} download>
+      <a className="resume-download" href="https://drive.google.com/file/d/1DAXhTkYSe0RoLVeGhwYIHeCJFfBTiXWW/view" target="_blank" download >
         <i class="fas fa-file-download" />
       </a>
       <p>Download Resumé</p>
